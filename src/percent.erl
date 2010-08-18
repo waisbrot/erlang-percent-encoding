5bfe36cc9f52e89fc3b1faf98265d7f7911542c4
Tim Fletcher <mail@tfletcher.com>
2010-08-18 12:39:59 +0100

    Decode '+' correctly to make url_decode compatible with url_encode.

--- src/percent.erl
+++ src/percent.erl
@@ -1,11 +1,11 @@
 -module(percent).
 
--export([url_encode/1, uri_encode/1, url_decode/1, uri_decode/1]).
+-export([url_encode/1, url_decode/1, uri_encode/1, uri_decode/1]).
 
 -define(is_alphanum(C), C >= $A, C =< $Z; C >= $a, C =< $z; C >= $0, C =< $9).
 
 %%
-%% Percent encoding as defined by the application/x-www-form-urlencoded
+%% Percent encoding/decoding as defined by the application/x-www-form-urlencoded
 %% content type (http://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1).
 %%
 
@@ -22,8 +22,21 @@ url_encode([X | T], Acc) ->
 url_encode([], Acc) ->
   Acc.
 
+url_decode(Str) when is_list(Str) ->
+  url_decode(Str, []).
+
+url_decode([$+ | T], Acc) ->
+  url_decode(T, [32 | Acc]);
+url_decode([$%, A, B | T], Acc) ->
+  Char = (hexchr_decode(A) bsl 4) + hexchr_decode(B),
+  url_decode(T, [Char | Acc]);
+url_decode([X | T], Acc) ->
+  url_decode(T, [X | Acc]);
+url_decode([], Acc) ->
+  lists:reverse(Acc, []).
+
 %%
-%% Percent encoding as defined by RFC 3986 (http://tools.ietf.org/html/rfc3986).
+%% Percent encoding/decoding as defined by RFC 3986 (http://tools.ietf.org/html/rfc3986).
 %%
 
 uri_encode(Str) when is_list(Str) ->
@@ -37,22 +50,15 @@ uri_encode([X | T], Acc) ->
 uri_encode([], Acc) ->
   Acc.
 
-%%
-%% Percent decoding.
-%%
-
-url_decode(Str) when is_list(Str) ->
-  url_decode(Str, []).
-
 uri_decode(Str) when is_list(Str) ->
-  url_decode(Str, []).
+  uri_decode(Str, []).
 
-url_decode([$%, A, B | T], Acc) ->
+uri_decode([$%, A, B | T], Acc) ->
   Char = (hexchr_decode(A) bsl 4) + hexchr_decode(B),
-  url_decode(T, [Char | Acc]);
-url_decode([X | T], Acc) ->
-  url_decode(T, [X | Acc]);
-url_decode([], Acc) ->
+  uri_decode(T, [Char | Acc]);
+uri_decode([X | T], Acc) ->
+  uri_decode(T, [X | Acc]);
+uri_decode([], Acc) ->
   lists:reverse(Acc, []).
 
 %%
